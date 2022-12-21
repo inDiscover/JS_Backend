694818bd251760891e3ad051bb94bb0c3cf673b8
myildizCH <meyildiz.de@gmail.com>
2022-12-21 16:57:06 +0100

    adds localStorage write and read logic

--- JavaScript Backend Development/FrontendForOurProject/typescript/TodoComponent.ts
+++ JavaScript Backend Development/FrontendForOurProject/typescript/TodoComponent.ts
@@ -1,4 +1,8 @@
+import { Todo } from "./TodoModel";
+
 export class TodoComponent {
+  private todoList: Todo[] = [];
+
   constructor(selector: string) {
     const template = `
   <div>
@@ -13,12 +17,22 @@ export class TodoComponent {
     const root = document.querySelector(selector);
     root?.insertAdjacentHTML("beforeend", template);
 
-    const addButton = document.querySelector("#add-button") as HTMLButtonElement;
+    const addButton = document.querySelector(
+      "#add-button"
+    ) as HTMLButtonElement;
     const todoInput = document.querySelector("#todo-input") as HTMLInputElement;
     const itemList = document.querySelector("#item-list") as HTMLUListElement;
 
+    // Read back from localstorage 
+    this.todoList = JSON.parse(localStorage.getItem("todolist") ?? "[]");
+    console.log(this.todoList);
+
     //* Dynamic add using button click
     addButton.addEventListener("click", () => {
+      //* Capture the todo text, create a Todo object and add to todoList
+      const todo = new Todo(todoInput?.value);
+      this.todoList.push(todo);
+
       //? Prepare the li element
       const item = `<li>${todoInput?.value}</li>`;
 
@@ -28,7 +42,10 @@ export class TodoComponent {
       //? Make input null again
       todoInput.value = "";
 
-    
+      console.log(this.todoList);
+
+      // Write the data to local storage
+      localStorage.setItem("todolist", JSON.stringify(this.todoList));
     });
   }
 }
