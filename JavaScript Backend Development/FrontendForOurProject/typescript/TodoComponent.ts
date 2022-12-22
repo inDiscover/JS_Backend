b7f641156c82d006294bd20df7ed83fcb994ce88
myildizCH <meyildiz.de@gmail.com>
2022-12-23 00:08:28 +0100

    refactor TodoComponent

--- JavaScript Backend Development/FrontendForOurProject/typescript/TodoComponent.ts
+++ JavaScript Backend Development/FrontendForOurProject/typescript/TodoComponent.ts
@@ -1,11 +1,56 @@
+//! M - V - CONTROLLER
+
 import { Todo } from "./TodoModel";
 import { TodoService } from "./TodoService";
 
 export class TodoComponent {
   private todoList: Todo[] = [];
-  todoService = new TodoService();
+  private todoService = new TodoService();
+  private itemList: HTMLUListElement;
+  private todoInput: HTMLInputElement;
 
   constructor(selector: string) {
+    //! M - VIEW - C
+    this.createView(document.querySelector(selector) ?? document.body);
+
+    //* Store <input>, <ul>, and <button> in instance variables for later use
+    const addButton = document.querySelector(
+      "#add-button"
+    ) as HTMLButtonElement;
+    this.todoInput = document.querySelector("#todo-input") as HTMLInputElement;
+    this.itemList = document.querySelector("#item-list") as HTMLUListElement;
+
+    // Read back from localstorage
+    // this.todoList = JSON.parse(localStorage.getItem("todolist") ?? "[]");
+    this.todoList = this.todoService.getAllTodos();
+    this.todoList.forEach((todo: Todo) => {
+      const item = `<li>${todo.todoItem}</li>`;
+      this.itemList.insertAdjacentHTML("beforeend", item);
+    });
+
+    console.log(this.todoList);
+
+    //* Dynamic add using button click
+    addButton?.addEventListener("click", () => {
+      this.renderTodo(this.todoService.createTodo(this.todoInput?.value));
+      this.todoInput.value = "";
+      //* Capture the todo text, create a Todo object and add to todoList
+      // const todo = new Todo(todoInput?.value);
+      const todo = this.todoService.createTodo(this.todoInput?.value);
+      this.todoList.push(todo);
+
+      //? Make input null again
+      this.todoInput.value = "";
+
+      console.log(this.todoList);
+
+      // Write the data to local storage
+      // localStorage.setItem("todolist", JSON.stringify(this.todoList));
+    });
+  }
+
+  private createView(root: HTMLElement) {
+    //* Process the given string and convert to DOM elements
     const template = `
   <div>
     <input id="todo-input" type="text" placeholder="Enter text here"/>
@@ -15,47 +60,35 @@ export class TodoComponent {
   <ul id="item-list"></ul>
   `;
 
-    //* Process the given string and convert to DOM elements
-    const root = document.querySelector(selector);
     root?.insertAdjacentHTML("beforeend", template);
+  }
 
-    const addButton = document.querySelector(
-      "#add-button"
-    ) as HTMLButtonElement;
-    const todoInput = document.querySelector("#todo-input") as HTMLInputElement;
-    const itemList = document.querySelector("#item-list") as HTMLUListElement;
+  private renderTodo(todo: Todo) {
+    const buttonStyle = `
+    color:red;
+    background-color:transparent;
+    border:none;
+    `;
 
-    // Read back from localstorage
-    // this.todoList = JSON.parse(localStorage.getItem("todolist") ?? "[]");
-    this.todoList = this.todoService.getAllTodos();
-    this.todoList.forEach((todo: Todo) => {
-      const item = `<li>${todo.todoItem}</li>`;
-      itemList.insertAdjacentHTML("beforeend", item);
-    });
+    //? Prepare the li element
+    const item = `<li id="todo-${todo.todoKey}">
+                    ${todo.todoItem}
+                      <button style='${buttonStyle}'>X</button>
+                    </li>`;
 
-    console.log(this.todoList);
+    //? Add it
+    this.itemList.insertAdjacentHTML("beforeend", item);
 
-    //* Dynamic add using button click
-    addButton.addEventListener("click", () => {
-      //* Capture the todo text, create a Todo object and add to todoList
-      // const todo = new Todo(todoInput?.value);
-      const todo = this.todoService.createTodo(todoInput?.value);
-      this.todoList.push(todo);
+    //* Find the recently added button and add click handler to it
+    this.itemList
+      .querySelector(`#todo-${todo.todoKey} > button`)
+      ?.addEventListener("click", () => {
+        //! Ask service to remove the todo
+        this.todoService.deleteTodo(todo.todoKey);
 
-      //? Prepare the li element
-      const item = `<li>${todoInput?.value}</li>`;
-
-      //? Add it
-      itemList.insertAdjacentHTML("beforeend", item);
-
-      //? Make input null again
-      todoInput.value = "";
-
-      console.log(this.todoList);
-
-      // Write the data to local storage
-      // localStorage.setItem("todolist", JSON.stringify(this.todoList));
-    });
+        //! Remove it from DOM Tree as well
+        this.itemList.querySelector(`#todo-${todo.todoKey}`)?.remove();
+      });
   }
 }
 
