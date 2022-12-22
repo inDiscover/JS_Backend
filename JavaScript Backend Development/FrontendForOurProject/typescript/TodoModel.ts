fe1aca73734f7457a5779bef813a2b245466c257
myildizCH <meyildiz.de@gmail.com>
2022-12-23 00:08:42 +0100

    create TodoModel

---
+++ JavaScript Backend Development/FrontendForOurProject/typescript/TodoModel.ts
@@ -0,0 +1,14 @@
+//! MODEL
+
+export class Todo {
+  public static lastKey = 0;
+  public todoKey: number;
+  public todoItem: string;
+  
+  constructor(todoItem: string) {
+    this.todoKey = ++Todo.lastKey;
+    this.todoItem = todoItem;
+  }
+
+}
+
